49503bbca9150bbd1ed1ef0adadf52c7c5033541
Denis Kruchinin <lestrigon17@yandex.ru>
2024-02-12 14:41:17 +0500

    Add atom gravitation

---
+++ assets/scripts/game/meta-atoms/atomGravitation.ts
@@ -0,0 +1,27 @@
+import { AtomBase } from "./atomBase";
+import { EAtomGravitationEvents } from "./types/eAtomGravitationEvents";
+import { EAtomType } from "./types/eAtomType";
+
+/**
+ * Events:
+ * OnDivingAffectChange (newState, oldState)
+ * OnAffectChange (newState, oldState)
+ */
+export class AtomGravitation extends AtomBase {
+    public get isDivingAffect(): boolean { return this.isDivingAffectInternal; }
+    public set isDivingAffect(state: boolean) {
+        this.emit(EAtomGravitationEvents.OnDivingAffectChange, state, this.isDivingAffectInternal);
+        this.isDivingAffectInternal = state;
+    }
+
+    public get isAffect(): boolean { return this.isAffectInternal; }
+    public set isAffect(state: boolean) {
+        this.emit(EAtomGravitationEvents.OnAffectChange, state, this.isAffectInternal);
+        this.isAffectInternal = state;
+    }
+
+    protected typeInternal: EAtomType = EAtomType.Gravitation;
+
+    private isDivingAffectInternal: boolean = false;
+    private isAffectInternal: boolean = true;
+}
---
+++ assets/scripts/game/meta-atoms/types/eAtomGravitationEvents.ts
@@ -0,0 +1,4 @@
+export enum EAtomGravitationEvents {
+    OnAffectChange = 'onAffectChange',
+    OnDivingAffectChange = 'onDivingAffectChange'
+}
