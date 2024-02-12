ab39e0fcde831a0a422798653b9606a8eda37b1d
Denis Kruchinin <lestrigon17@yandex.ru>
2024-02-12 14:29:15 +0500

    Prepare for service system

---
+++ assets/scripts/services/index.ts
@@ -0,0 +1,5 @@
+/// <reference path="./types/types.services.d.ts" />
+
+import { createMetaStorage } from "../utils/metaStorage";
+
+export const Services = createMetaStorage<typeof Types.Services>();
---
+++ assets/scripts/services/types/types.services.d.ts
@@ -0,0 +1,3 @@
+declare namespace Types.Services {
+    export const __ctor__: never;
+}
