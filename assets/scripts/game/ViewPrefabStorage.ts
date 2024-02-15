cc66c5393e1c63971947f0318ff89bce0d882bf1
Denis Kruchinin <lestrigon17@yandex.ru>
2024-02-15 19:21:14 +0500

    Add view prefab storage

---
+++ assets/scripts/game/ViewPrefabStorage.ts
@@ -0,0 +1,17 @@
+import { Component, Enum, Prefab, _decorator } from "cc";
+import { EGItemType } from "./game-items/types/eGItemTypes";
+
+const {ccclass, property} = _decorator;
+
+const gameItemsEnum = Enum(EGItemType);
+
+@ccclass("ViewPrefabStorageData")
+class ViewPrefabStorageData {
+    @property({type: gameItemsEnum}) type: EGItemType = EGItemType.BaseItem;
+    @property(Prefab) prefab!: Prefab;
+}
+
+@ccclass("ViewPrefabStorage")
+export class ViewPrefabStorage extends Component {
+    @property([ViewPrefabStorageData]) prefabs: ViewPrefabStorageData[] = [];
+}
--- assets/scripts/game/game-items/types/eGItemTypes.ts
+++ assets/scripts/game/game-items/types/eGItemTypes.ts
@@ -1,4 +1,4 @@
 export enum EGItemType {
-    BaseItem = "baseItem",
-    ColorItem = "colorItem",
+    BaseItem,
+    ColorItem,
 }
